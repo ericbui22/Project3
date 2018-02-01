3c1af6f13ca8fa3dd0a93e43745e0fb7a2d63182
Eric Bui <ericbui221@gmail.com>
2018-01-31 20:39:36 -0600

    Add project files.

---
+++ Project3/flowcontrol.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	char cont;
+	srand(time(NULL));
+
+	int num1 = rand() % 10 + 1;
+	cout << "your random number is " << num1;
+	cin >> cont;
+
+	return 0;
+}
